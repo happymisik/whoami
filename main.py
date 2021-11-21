defa6ce534f382c0d2dddfc4acc7a7842a3c3b98
misik <happymisik@naver.com>
2021-11-21 13:07:32 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+# -*-coding:utf-8-*-
+import pandas as pd
+from flask import Flask, request, json
+import random
+
+app = Flask(__name__)
+
+commonResponse = {
+    'version': '2.0',
+    'resultCode': 'OK',
+    'output': {}
+}
+
+df = pd.read_csv("dinosourquize.csv", encoding='utf-8')
+num = 0
+list_from_df = df.values.tolist()
+for data in list_from_df:
+    print("{}번 문제 : {}".format(num  + 1, list_from_df[num][0]))
+    print("{}번문제의 힌트1 : {}".format(num + 1, list_from_df[num][1]))
+    print("{}번문제의 힌트2 : {}".format(num + 1, list_from_df[num][2]))
+    print("{}번문제의 힌트3 : {}".format(num + 1, list_from_df[num][3]))
+    print("{}번문제의 힌트4 : {}".format(num + 1, list_from_df[num][4]))
+    print("{}번문제의 힌트5 : {}".format(num + 1, list_from_df[num][5]))
+    num = num + 1
+    # print("{}번 문제 : {}".format(num+1,list_from_df[num][0]))
+
+print(list_from_df[0][0])
+
+
+def getUtteranceParameter():
+    data = request.get_json()
+    print(data)
+    return data['action']['parameters']
+
+
+@app.route('/')
+def index():
+    return 'Hello Flask'
+
+
+@app.route('/dinosaur_name', methods=['POST'])
+def createQuize():
+    response = commonResponse
+    randomNumber = random.randint(0, 1)
+    print(randomNumber)
+    print(response)
+
+    return json.dumps(response)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5500, debug=True)
+
